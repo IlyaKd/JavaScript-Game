78fc8bfd92fa18275a14d9dbf6543113d7c6bf37
IlyaKd <ilyakd@protonmail.com>
2020-09-14 15:42:33 +0300

    add project

---
+++ js/script.js
@@ -0,0 +1,97 @@
+const allLevels = document.querySelectorAll('.level');
+const startGameBtn = document.querySelector('.button');
+const main = document.querySelector('.main');
+const cardsBox = document.querySelector('.cards-box');
+
+const chooseLevel = event => {
+    allLevels.forEach((item) => item.classList.remove('level_active'));
+    event.currentTarget.classList.add('level_active');
+};
+
+allLevels.forEach((item) => item.addEventListener('click', chooseLevel));
+
+const getNumberCards = currentLevel => {
+    switch (currentLevel) {
+        case 'simple':
+            cardsBox.classList.add('three_cards');
+            return 3;
+            break;
+        case 'average':
+            cardsBox.classList.add('six_cards');
+            return 6;
+            break;
+        case 'difficult':
+            cardsBox.classList.add('ten_cards');
+            return 10;
+            break;
+    }
+};
+
+const createCards = currentLevelForStart => {
+    for (let i = 0; i < currentLevelForStart; i++) {
+        const card = document.createElement('div');
+        const cardInner = document.createElement('div');
+        const cardFront = document.createElement('div');
+        const cardBack = document.createElement('div');
+
+        card.classList.add('card');
+        cardInner.classList.add('card__inner', 'card__inner_hover');
+        cardFront.classList.add('card__front');
+        cardBack.classList.add('card__back');
+
+        cardsBox.append(card);
+        card.append(cardInner);
+        cardInner.append(cardFront);
+        cardInner.append(cardBack);
+    };
+};
+
+const createBug = currentLevelForStart => {
+    let bug = Math.floor(Math.random() * currentLevelForStart);
+    const cardsBack = document.querySelectorAll('.card__back');
+
+    for (let i = 0; i < currentLevelForStart; i++) {
+        if (i === bug) {
+            cardsBack[i].classList.remove('card__back');
+            cardsBack[i].classList.add('card__bug');
+        }
+    };
+};
+
+const deleteCards = () => {
+    cardsBox.classList.remove('three_cards', 'six_cards', 'ten_cards');
+    while (cardsBox.firstChild) {
+        cardsBox.removeChild(cardsBox.firstChild);
+    };
+};
+
+const flipCard = () => {
+    let inGame = true;
+    const allCards = document.querySelectorAll('.card__inner');
+    allCards.forEach((item) => item.addEventListener('click', () => {
+        if(inGame) {
+            item.classList.add('flipped');
+            item.classList.remove('card__inner_hover');
+            inGame = false;
+        } else {
+            main.classList.remove('none');
+            cardsBox.classList.add('none');
+            inGame = true;
+            deleteCards();
+        };
+    }));
+};
+
+const startGame = () => {
+    let currentLevel = document.querySelector('.level_active').firstElementChild.getAttribute('id');
+    let currentLevelForStart = getNumberCards(currentLevel);
+
+    main.classList.add('none');
+    cardsBox.classList.remove('none');
+
+    createCards(currentLevelForStart);
+    createBug(currentLevelForStart);
+    flipCard();
+};
+
+startGameBtn.addEventListener('click', startGame);
